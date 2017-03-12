265af5c48451bb928732202adacfdd58c32cc92f
david850519 <david850519@gmail.com>
2017-03-12 20:48:50 +0800

    Initial commit

---
+++ app/src/main/java/net/iremix/music_test/MusicController.java
@@ -0,0 +1,17 @@
+package net.iremix.music_test;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by peter on 2017/3/8.
+ */
+
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
